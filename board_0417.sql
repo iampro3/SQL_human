559b83417fcb3b0ad50a37e8c40b8a307b4f822a
iampro3 <lucky1977@daum.net>
2023-04-17 16:56:23 +0900

    0417_sql /erd cloud 에서 생성한 sql 테이블

---
+++ board_0417.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE "survey" (
+	"survey_id"	number(10)		NOT NULL,
+	"name"	varchar2(200)		NULL,
+	"desc"	varchar2(4000)		NULL
+);
+
+CREATE TABLE "question" (
+	"q_id"	number(10)		NOT NULL,
+	"question"	varchar2(200)		NULL,
+	"survey_id"	number(10)		NOT NULL,
+	"qt_id"	number(10)		NOT NULL
+);
+
+CREATE TABLE "response" (
+	"r_id"	number(10)		NOT NULL,
+	"response"	varchar2(4000)		NULL,
+	"q_id"	number(10)		NOT NULL
+);
+
+CREATE TABLE "question_type" (
+	"qt_id"	number(10)		NOT NULL,
+	"q_type"	varchar2(2)		NULL
+);
+
+ALTER TABLE "survey" ADD CONSTRAINT "PK_SURVEY" PRIMARY KEY (
+	"survey_id"
+);
+
+ALTER TABLE "question" ADD CONSTRAINT "PK_QUESTION" PRIMARY KEY (
+	"q_id"
+);
+
+ALTER TABLE "response" ADD CONSTRAINT "PK_RESPONSE" PRIMARY KEY (
+	"r_id"
+);
+
+ALTER TABLE "question_type" ADD CONSTRAINT "PK_QUESTION_TYPE" PRIMARY KEY (
+	"qt_id"
+);
+
+
+select * from tab;
